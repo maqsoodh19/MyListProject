a521e1cb626e39f39fd1f3e35046ee052b6818a8
Maqsood <maqsoodh19@gmail.com>
2017-10-02 17:50:24 +0500

    Array adapter

--- app/src/main/java/com/example/maqso/mylistproject/welcome.java
+++ app/src/main/java/com/example/maqso/mylistproject/welcome.java
@@ -2,6 +2,10 @@ package com.example.maqso.mylistproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import static android.R.attr.data;
 
 public class welcome extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class welcome extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        String data[] = {"Salman","Gohhar","ABC","ZYX","Hassan"};
+
+        ListView listView = (ListView) findViewById(R.id.listview);
+
+        ArrayAdapter<String> list = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,data);
+        listView.setAdapter(list);
+
     }
 }
